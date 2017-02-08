6b24288fb0d2636dcaaba2134c2f006c3fda444c
danny8508021 <danny8508021@gmail.com>
2017-02-09 01:48:46 +0800

    Add ModelCount Ver 1.

--- src/ssat/core/ssatModelCount.cc
+++ src/ssat/core/ssatModelCount.cc
@@ -52,7 +52,32 @@ using namespace std;
 double
 SsatSolver::allSatModelCount( Solver * s , const vec<Lit> & eLits , double curVal )
 {
-   return 0.0;
+   vec<Lit> rLits ( _rootVars[1].size() ), rHits;
+   vec<CRef> bkClas;
+   double count = 0.0, prob = 1;
+
+   while ( s->solve( eLits ) ) {
+      for ( int i = 0 ; i < _rootVars[1].size() ; ++i )
+         rLits[i] = ( s->modelValue(_rootVars[1][i]) == l_True ) ? mkLit(_rootVars[1][i]) : ~mkLit(_rootVars[1][i]);
+      
+      // miniHitSet( s , rLits , rHits );       // get rHits
+
+      for ( int i = 0 ; i < rHits.size() ; ++i )
+         prob *= _quan[var(rHits[i])];
+      
+      count += prob;
+      if ( count >= 1 - curVal )
+         break;
+      prob = 1;
+
+      s->addClause( rHits );
+      bkClas.push( s->clauses[s->nClauses()-1] );
+   }
+
+   for ( int i = 0 ; i < bkClas.size() ; ++i )
+      s->removeClause( bkClas[i] );
+
+   return 1 - count;
 }
 
 ////////////////////////////////////////////////////////////////////////
