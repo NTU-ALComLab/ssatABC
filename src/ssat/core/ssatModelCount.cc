546d7d1fb6d354bdc763be25794b8982796f125c
nianze <nianzelee@gmail.com>
2017-02-09 11:16:08 +0800

    how to delete clause

--- src/ssat/core/ssatModelCount.cc
+++ src/ssat/core/ssatModelCount.cc
@@ -52,31 +52,32 @@ using namespace std;
 double
 SsatSolver::allSatModelCount( Solver * s , const vec<Lit> & eLits , double curVal )
 {
-   vec<Lit> rLits ( _rootVars[1].size() ), rHits;
-   vec<CRef> bkClas;
-   double count = 0.0, prob = 1;
+   vec<Lit> rLits ( _rootVars[1].size() ) , rHits , bkCla;
+   vec<CRef> bkClaRef;
+   double count = 0.0 , prob;
 
    while ( s->solve( eLits ) ) {
       for ( int i = 0 ; i < _rootVars[1].size() ; ++i )
          rLits[i] = ( s->modelValue(_rootVars[1][i]) == l_True ) ? mkLit(_rootVars[1][i]) : ~mkLit(_rootVars[1][i]);
-      
+     
+      // TODO: try to drop some rLits
       // miniHitSet( s , rLits , rHits );       // get rHits
-
+      // use rLits to debug
+      rLits.copyTo( rHits );
+      
+      prob = 1;
       for ( int i = 0 ; i < rHits.size() ; ++i )
-         prob *= _quan[var(rHits[i])];
+         prob *= sign(rHits[i]) ? 1 - _quan[var(rHits[i])] : _quan[var(rHits[i])];
       
       count += prob;
-      if ( count >= 1 - curVal )
-         break;
-      prob = 1;
-
-      s->addClause( rHits );
-      bkClas.push( s->clauses[s->nClauses()-1] );
+      if ( count >= 1 - curVal ) break;
+      bkCla.clear();
+      for ( int i = 0 ; i < rHits.size() ; ++i ) bkCla.push( ~rHits[i] );
+      s->addClause( bkCla );
+      bkClaRef.push( s->clauses[s->nClauses()-1] );
    }
-
-   for ( int i = 0 ; i < bkClas.size() ; ++i )
-      s->removeClause( bkClas[i] );
-
+   for ( int i = 0 ; i < bkClaRef.size() ; ++i )
+      s->removeClause( bkClaRef[i] );
    return 1 - count;
 }
 
