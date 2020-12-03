17d6ed078e85c5f523423715e22771e00a885f0e
nianze <nianzelee@gmail.com>
2020-12-03 12:47:44 +0800

    Refactor ssat_test.cc
    Add a common subroutine which takes:
    - a test case
    - solver parameters
    - a verdict

--- src/ssat/core/ssat_test.cc
+++ src/ssat/core/ssat_test.cc
@@ -23,68 +23,55 @@ using namespace Minisat;
 
 extern void initParams(Ssat_Params_t*);
 
+static void runTestWithParamsAndVerdict(const char* pTestCase,
+                                        Ssat_Params_t* pParams,
+                                        const double pVerdict) {
+  abctime clk = Abc_Clock();
+  printf("  > Test case: %s\n", pTestCase);
+  gzFile in = gzopen(pTestCase, "rb");
+  SsatSolver* pSsat = new SsatSolver(pParams->fTimer, pParams->fVerbose);
+  pSsat->readSSAT(in);
+  gzclose(in);
+  pSsat->solveSsat(pParams);
+  double answer = pSsat->exactSatProb();
+  REQUIRE(answer == Approx(pVerdict));
+  delete pSsat;
+  Abc_PrintTime(1, "  > Time", Abc_Clock() - clk);
+  printf("-------------------------------------------\n");
+}
+
 ////////////////////////////////////////////////////////////////////////
 ///                     FUNCTION DEFINITIONS                         ///
 ////////////////////////////////////////////////////////////////////////
 
-TEST_CASE("toliet_1", "[planning]") {
-  const char* targetFile =
+TEST_CASE("toilet_1", "[planning]") {
+  const char* testCase =
       "benchmarks/ssatER/planning/ToiletA/sdimacs/toilet_a_08_01.2.sdimacs";
-  abctime clk = Abc_Clock();
+  double verdict = 0.0078125;
   Ssat_Params_t Params, *pParams = &Params;
   initParams(pParams);
   pParams->fPart = false;
-  printf("  > Testing target: %s\n", targetFile);
-  gzFile in = gzopen(targetFile, "rb");
-  SsatSolver* pSsat = new SsatSolver(pParams->fTimer, pParams->fVerbose);
-  pSsat->readSSAT(in);
-  gzclose(in);
-  pSsat->solveSsat(pParams);
-  double answer = pSsat->lowerBound();
-  REQUIRE(answer == Approx(0.0078125));
-  delete pSsat;
-  Abc_PrintTime(1, "  > Elasped time", Abc_Clock() - clk);
-  printf("-------------------------------------------\n");
+  runTestWithParamsAndVerdict(testCase, pParams, verdict);
 }
 
-TEST_CASE("toliet_2", "[planning]") {
-  const char* targetFile =
+TEST_CASE("toilet_2", "[planning]") {
+  const char* testCase =
       "benchmarks/ssatER/planning/ToiletA/sdimacs/toilet_a_08_01.4.sdimacs";
-  abctime clk = Abc_Clock();
+  double verdict = 0.015625;
   Ssat_Params_t Params, *pParams = &Params;
   initParams(pParams);
-  printf("  > Testing target: %s\n", targetFile);
-  gzFile in = gzopen(targetFile, "rb");
-  SsatSolver* pSsat = new SsatSolver(pParams->fTimer, pParams->fVerbose);
-  pSsat->readSSAT(in);
-  gzclose(in);
-  pSsat->solveSsat(pParams);
-  double answer = pSsat->lowerBound();
-  REQUIRE(answer == Approx(0.015625));
-  delete pSsat;
-  Abc_PrintTime(1, "  > Elasped time", Abc_Clock() - clk);
-  printf("-------------------------------------------\n");
+  runTestWithParamsAndVerdict(testCase, pParams, verdict);
 }
 
-TEST_CASE("toliet_3", "[planning]") {
-  const char* targetFile =
+TEST_CASE("toilet_3", "[planning]") {
+  const char* testCase =
       "benchmarks/ssatER/planning/ToiletA/sdimacs/toilet_a_10_01.2.sdimacs";
-  abctime clk = Abc_Clock();
+  double verdict = 0.001953125;
   Ssat_Params_t Params, *pParams = &Params;
   initParams(pParams);
   pParams->fPart = false;
   pParams->fCkt = false;
-  printf("  > Testing target: %s\n", targetFile);
-  gzFile in = gzopen(targetFile, "rb");
-  SsatSolver* pSsat = new SsatSolver(pParams->fTimer, pParams->fVerbose);
-  pSsat->readSSAT(in);
-  gzclose(in);
-  pSsat->solveSsat(pParams);
-  double answer = pSsat->lowerBound();
-  REQUIRE(answer == Approx(0.001953125));
-  delete pSsat;
-  Abc_PrintTime(1, "  > Elasped time", Abc_Clock() - clk);
-  printf("-------------------------------------------\n");
+  runTestWithParamsAndVerdict(testCase, pParams, verdict);
 }
 
 ////////////////////////////////////////////////////////////////////////
