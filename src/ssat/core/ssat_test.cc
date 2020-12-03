e309f11062f28ffdb2788b5bbb1399d3c4541105
nianze <nianzelee@gmail.com>
2020-12-03 17:32:13 +0800

    Add three test cases for sand-castle family and RE-SSAT

--- src/ssat/core/ssat_test.cc
+++ src/ssat/core/ssat_test.cc
@@ -48,7 +48,7 @@ static void runTestWithParamsAndVerdict(const char* pTestCase,
 TEST_CASE("toilet_1", "[planning]") {
   const char* testCase =
       "benchmarks/ssatER/planning/ToiletA/sdimacs/toilet_a_08_01.2.sdimacs";
-  double verdict = 0.0078125;
+  double verdict = 7.812500e-03;
   Ssat_Params_t Params, *pParams = &Params;
   initParams(pParams);
   pParams->fPart = false;
@@ -58,7 +58,7 @@ TEST_CASE("toilet_1", "[planning]") {
 TEST_CASE("toilet_2", "[planning]") {
   const char* testCase =
       "benchmarks/ssatER/planning/ToiletA/sdimacs/toilet_a_08_01.4.sdimacs";
-  double verdict = 0.015625;
+  double verdict = 1.562500e-02;
   Ssat_Params_t Params, *pParams = &Params;
   initParams(pParams);
   runTestWithParamsAndVerdict(testCase, pParams, verdict);
@@ -67,11 +67,41 @@ TEST_CASE("toilet_2", "[planning]") {
 TEST_CASE("toilet_3", "[planning]") {
   const char* testCase =
       "benchmarks/ssatER/planning/ToiletA/sdimacs/toilet_a_10_01.2.sdimacs";
-  double verdict = 0.001953125;
+  double verdict = 1.953125e-03;
   Ssat_Params_t Params, *pParams = &Params;
   initParams(pParams);
   pParams->fPart = false;
   pParams->fCkt = false;
+  runTestWithParamsAndVerdict(testCase, pParams, verdict);
+}
+
+TEST_CASE("sand-castle-10", "[planning]") {
+  const char* testCase =
+      "benchmarks/ssatER/planning/sand-castle/sdimacs/SC-10.sdimacs";
+  double verdict = 9.668871e-01;
+  Ssat_Params_t Params, *pParams = &Params;
+  initParams(pParams);
+  runTestWithParamsAndVerdict(testCase, pParams, verdict);
+}
+
+TEST_CASE("sand-castle-10-gsp", "[planning]") {
+  const char* testCase =
+      "benchmarks/ssatER/planning/sand-castle/sdimacs/SC-10.sdimacs";
+  double verdict = 9.668871e-01;
+  Ssat_Params_t Params, *pParams = &Params;
+  initParams(pParams);
+  pParams->fGreedy = false;
+  pParams->fSub = false;
+  pParams->fPart = false;
+  runTestWithParamsAndVerdict(testCase, pParams, verdict);
+}
+
+TEST_CASE("random-RE", "[planning]") {
+  const char* testCase =
+      "benchmarks/ssatRE/random/3CNF/sdimacs/rand-3-40-120-20.165.sdimacs";
+  double verdict = 1.207352e-03;
+  Ssat_Params_t Params, *pParams = &Params;
+  initParams(pParams);
   runTestWithParamsAndVerdict(testCase, pParams, verdict);
 }
 
