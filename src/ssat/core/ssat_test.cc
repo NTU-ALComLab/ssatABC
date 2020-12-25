c1996549cfcb5a340a1e6c3913d8a3c848efbb2c
nianze <nianzelee@gmail.com>
2020-12-25 16:28:32 +0800

    Add more regression tests
    At least one instance from each family.

--- src/ssat/core/ssat_test.cc
+++ src/ssat/core/ssat_test.cc
@@ -99,6 +99,86 @@ TEST_CASE("sand-castle-10-gsp", "[planning]") {
   runTestWithParamsAndVerdict(testCase, pParams, verdict);
 }
 
+TEST_CASE("conformant-cube", "[planning]") {
+  const char* testCase =
+      "test/test-cases/ssatER/planning/conformant/sdimacs/"
+      "cube_c3_ser--opt-6_.sdimacs";
+  double verdict = 1;
+  Ssat_Params_t Params, *pParams = &Params;
+  initParams(pParams);
+  runTestWithParamsAndVerdict(testCase, pParams, verdict);
+}
+
+TEST_CASE("conformant-cube-gsp", "[planning]") {
+  const char* testCase =
+      "test/test-cases/ssatER/planning/conformant/sdimacs/"
+      "cube_c3_ser--opt-6_.sdimacs";
+  double verdict = 1;
+  Ssat_Params_t Params, *pParams = &Params;
+  initParams(pParams);
+  pParams->fGreedy = false;
+  pParams->fSub = false;
+  pParams->fPart = false;
+  runTestWithParamsAndVerdict(testCase, pParams, verdict);
+}
+
+TEST_CASE("QIF-backdoor", "[planning]") {
+  const char* testCase =
+      "test/test-cases/ssatER/MaxCount/sdimacs/QIF-backdoor-32-24.sdimacs";
+  double verdict = 1;
+  Ssat_Params_t Params, *pParams = &Params;
+  initParams(pParams);
+  runTestWithParamsAndVerdict(testCase, pParams, verdict);
+}
+
+TEST_CASE("QIF-backdoor-gsp", "[planning]") {
+  const char* testCase =
+      "test/test-cases/ssatER/MaxCount/sdimacs/QIF-backdoor-32-24.sdimacs";
+  double verdict = 1;
+  Ssat_Params_t Params, *pParams = &Params;
+  initParams(pParams);
+  pParams->fGreedy = false;
+  pParams->fSub = false;
+  pParams->fPart = false;
+  runTestWithParamsAndVerdict(testCase, pParams, verdict);
+}
+
+TEST_CASE("MPEC-c499", "[planning]") {
+  const char* testCase = "test/test-cases/ssatER/MPEC/sdimacs/c499-er.sdimacs";
+  double verdict = 2.343750e-01;
+  Ssat_Params_t Params, *pParams = &Params;
+  initParams(pParams);
+  runTestWithParamsAndVerdict(testCase, pParams, verdict);
+}
+
+TEST_CASE("MPEC-c499-gsp", "[planning]") {
+  const char* testCase = "test/test-cases/ssatER/MPEC/sdimacs/c499-er.sdimacs";
+  double verdict = 2.343750e-01;
+  Ssat_Params_t Params, *pParams = &Params;
+  initParams(pParams);
+  pParams->fGreedy = false;
+  pParams->fSub = false;
+  pParams->fPart = false;
+  runTestWithParamsAndVerdict(testCase, pParams, verdict);
+}
+
+TEST_CASE("strategic-company-20.4", "[planning]") {
+  const char* testCase =
+      "test/test-cases/ssatRE/stracomp/sdimacs/x20.4.sdimacs";
+  double verdict = 9.999971e-01;
+  Ssat_Params_t Params, *pParams = &Params;
+  initParams(pParams);
+  runTestWithParamsAndVerdict(testCase, pParams, verdict);
+}
+
+TEST_CASE("PEC-c3540", "[planning]") {
+  const char* testCase = "test/test-cases/ssatRE/PEC/sdimacs/c3540_re.sdimacs";
+  double verdict = 3.417969e-03;
+  Ssat_Params_t Params, *pParams = &Params;
+  initParams(pParams);
+  runTestWithParamsAndVerdict(testCase, pParams, verdict);
+}
+
 TEST_CASE("random-RE", "[planning]") {
   const char* testCase =
       "test/test-cases/ssatRE/random/3CNF/sdimacs/rand-3-40-120-20.165.sdimacs";
